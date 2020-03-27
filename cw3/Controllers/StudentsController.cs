0f488acf8d8d30350600ce0ffb0be2a18d4c231b
Kamil <Kamil@DESKTOP-ALQP0VL>
2020-03-27 21:55:26 +0100

    Dodaj pliki projektów.

---
+++ cw3/Controllers/StudentsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using cw3.Models;
+using cw3.DAL;
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    
+    public class StudentsController : ControllerBase
+    {
+        private IDbService _dbService;
+        public StudentsController(IDbService service)
+        {
+            _dbService = service;
+        }
+
+
+        [HttpGet]
+        public string GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            
+            return Ok(student); 
+        }
+
+        
+    }
+}
---
+++ cw3/DAL/MockDbService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.Models;
+
+namespace cw3.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students = new List<Student>
+        {
+            new Student{IdStudent=1, FirstName="Jan", LastName="Kowalski", IndexNumber="s1234"},
+            new Student{IdStudent=2, FirstName="Anna", LastName="Malewski", IndexNumber="s2342"},
+            new Student{IdStudent=3, FirstName="Krzysztof", LastName="Andrzejewicz", IndexNumber="s5432"}
+        };
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
